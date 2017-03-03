c1db6addf9e0a101c53a14cbc27ac2de89163225
yosmoc <motihara@gmail.com>
2017-03-03 20:11:40 +0100

    no use anymore git branch B alias

--- roles/oh-my-zsh/templates/general.zsh
+++ roles/oh-my-zsh/templates/general.zsh
@@ -2,6 +2,5 @@ export LANG=en_US.UTF-8
 alias ls="ls -a --color=auto"
 alias t="tig"
 alias ta="tig --all"
-alias -g B='`git branch | percol | sed -e "s/^\*[ ]*//g"`'
 export PATH=$PATH:~/tools
 export EDITOR=vim
