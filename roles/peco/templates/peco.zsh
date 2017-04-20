aaeaa2b09b68f97409de5c7bb9d8963954ba69a7
yosmoc <motihara@gmail.com>
2017-04-20 15:07:05 +0200

    move peco-select-tmux-session into peco.zsh

--- roles/peco/templates/peco.zsh
+++ roles/peco/templates/peco.zsh
@@ -25,3 +25,25 @@ function peco-src () {
 }
 zle -N peco-src
 bindkey '^]' peco-src
+
+function peco-select-tmux-session () {
+  # get the IDs
+  ID="`tmux list-sessions`"
+  if [[ -z "$ID" ]]; then
+    tmux new-session
+  fi
+  create_new_session="Create New Session"
+  ID="$ID\n${create_new_session}:"
+  ID="`echo $ID | peco | cut -d: -f1`"
+  if [[ "$ID" = "${create_new_session}" ]]; then
+    tmux new-session
+  elif [[ -n "$ID" ]]; then
+    tmux attach-session -t "$ID"
+  else
+    :  # Start terminal normally
+  fi
+}
+
+if [[ ! -n $TMUX ]]; then
+  peco-select-tmux-session
+fi
